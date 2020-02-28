94d0ba4d3b9e231e5c34eaaaf8e1ef3704f5f559
aleksandrmoroz <aleksandr.moroz@localize.city>
2020-02-28 11:54:24 +0200

    course project base

--- angular-proj/src/app/recipes/recipe-detail/recipe-detail.component.spec.ts
+++ angular-proj/src/app/recipes/recipe-detail/recipe-detail.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SucceedAlertComponent } from './succeed-alert.component';
+import { RecipeDetailComponent } from './recipe-detail.component';
 
-describe('SucceedAlertComponent', () => {
-  let component: SucceedAlertComponent;
-  let fixture: ComponentFixture<SucceedAlertComponent>;
+describe('RecipeDetailComponent', () => {
+  let component: RecipeDetailComponent;
+  let fixture: ComponentFixture<RecipeDetailComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SucceedAlertComponent ]
+      declarations: [ RecipeDetailComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SucceedAlertComponent);
+    fixture = TestBed.createComponent(RecipeDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
